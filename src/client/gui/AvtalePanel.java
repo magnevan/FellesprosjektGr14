7abc75a361fe3288923d4a17c4f91bc1323f1f7f
Magne Vikjord <mvikjord@gmail.com>
2012-03-14 13:59:31 +0100

    la til vertical layout manager

--- src/client/gui/AvtalePanel.java
+++ src/client/gui/AvtalePanel.java
@@ -1,30 +1,39 @@
 package client.gui;
 
-import java.awt.Dimension;
+import java.awt.Color;
 
-import javax.swing.Box;
-import javax.swing.BoxLayout;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 public class AvtalePanel extends JPanel {
 	
 	private final JTextField tittelText;
+//	private final JComboBox moteromComboBox;
+//	private final JTextField moteromText;
 	
 	public AvtalePanel() {
-		super();
-		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		super(new VerticalLayout(5,SwingConstants.LEFT));
+
 		
 		this.add(new JLabel("Tittel:"));
-		tittelText = new JTextField(25);
-		JPanel test = new JPanel();
-		test.add(tittelText);
-		this.add(test);
+		tittelText = new JTextField(20);
+		this.add(tittelText);
 		
-		JPanel fratilPanel = new JPanel();
-//		this.add(Box.createVerticalGlue());
-		this.add(new JLabel("test"));
+		
+	}
+	
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("test");
+		
+		JPanel content = new AvtalePanel();
+		frame.setContentPane(content);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		frame.pack();
+		frame.setVisible(true);
 	}
 
 }
