8e177c2e55c1e9242da4cba346866ca88b30349c
unknown <endre_ra@hotmail.com>
2012-03-16 11:51:59 +0100

    lol

--- src/client/gui/avtale/AppointmentPanel.java
+++ src/client/gui/avtale/AppointmentPanel.java
@@ -5,7 +5,10 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 
 import javax.swing.ImageIcon;
@@ -15,35 +18,21 @@ import javax.swing.JPanel;
 
 
 import client.gui.week.WeekView;
+import client.model.InvitationModel;
+import client.model.MeetingModel;
 import client.model.MeetingRoomModel;
 
-public class AppointmentPanel extends JPanel {
+public class AppointmentPanel extends JPanel implements PropertyChangeListener {
 
-	private String name, ownerName, location;
+
 	private GridBagConstraints c;
-	private Calendar timeFrom, timeTo;
 	private JLabel nameLabel, ownerLabel, timeLabel, iconLabel, locationLabel;
 	private Color color;
-	private ImageIcon typeIcon;
-	//private Avtale model;
+	private MeetingModel model;
 	
 	
-	public AppointmentPanel(String name, String ownerName, MeetingRoomModel room, String location, Calendar timeFrom, Calendar timeTo ){
+	public AppointmentPanel(MeetingModel MM){
 		
-		//model = null;
-		this.name = name;
-		this.ownerName = ownerName;
-		this.location = location;
-		this.timeFrom = timeFrom;
-		this.timeTo = timeTo;
-		
-		if(location != null){this.location = location;}
-		else{this.location = room.getRoomNumber();}
-		
-		
-		
-		typeIcon = new ImageIcon("src/resources/avtaleMini.png");
-		name = "Ny avtale";
 		
 		color=new Color(0,100,255);
 		setBackground(color);
@@ -54,34 +43,106 @@ public class AppointmentPanel extends JPanel {
     	c.gridx = 0;
     	c.gridy = 0;
     	c.fill = c.HORIZONTAL;
-	    nameLabel = new JLabel(name);
-	    nameLabel.setFont(new Font("Dialog", 1, 17));
+	    nameLabel = new JLabel();
 		add(nameLabel,c);
 		
 		c.gridy = 1;
-	    locationLabel = new JLabel(location);
+	    locationLabel = new JLabel();
 		add (locationLabel,c);
 		
 		c.gridy = 2;
-	    timeLabel = new JLabel(timeToString(timeFrom,timeTo));
+	    timeLabel = new JLabel();
 		add (timeLabel,c);
-		
-		
 		
 		c.gridy = 3;
 	    iconLabel = new JLabel();
-	    iconLabel.setIcon(typeIcon);
 		add(iconLabel,c);
 		
 		
 		c.gridy = 4;
-	    ownerLabel = new JLabel(ownerName);
+	    ownerLabel = new JLabel();
 		add(ownerLabel,c);
 		
-		setAppointmentTime(timeFrom, timeTo);
+		setModel(MM);
 
 	}
 	
+
+	public void setModel(MeetingModel model){
+		this.model = model;
+		
+		//Sletting av avtale
+		if(!model.isActive()){
+			//metode som fjerner AppointmentPanel
+			return;
+		}
+		
+		nameLabel.setText(model.getName());
+
+		if(model.getLocation() != null){locationLabel.setText(model.getLocation());}
+		else{locationLabel.setText(model.getRoom().getRoomNumber());}
+			
+		timeLabel.setText((timeToString(model.getTimeFrom(), model.getTimeTo())));
+		iconLabel.setIcon(getIcon(model.getInvitations()));
+		ownerLabel.setText(model.getOwner().getFullName());
+		
+		setAppointmentTime(model.getTimeFrom(), model.getTimeTo());
+		
+	}
+		
+	public MeetingModel getModel(){
+		return model;
+	}
+	
+	@Override
+	public void propertyChange(PropertyChangeEvent event) {
+		final String name = event.getPropertyName();
+		
+		if (name == MeetingModel.NAME_PROPERTY){
+			nameLabel.setText(model.getName());
+		}
+		if (name == MeetingModel.ROOM_PROPERTY){
+			locationLabel.setText(model.getRoom().getRoomNumber());
+		}
+		if (name == MeetingModel.LOCATION_PROPERTY){
+			locationLabel.setText(model.getLocation());
+		}
+		if (name == MeetingModel.TIME_FROM_PROPERTY){
+			timeLabel.setText((timeToString(model.getTimeFrom(), model.getTimeTo())));
+			setAppointmentTime(model.getTimeFrom(), model.getTimeTo());
+		}
+		if (name == MeetingModel.TIME_TO_PROPERTY){
+			timeLabel.setText((timeToString(model.getTimeFrom(), model.getTimeTo())));
+			setAppointmentTime(model.getTimeFrom(), model.getTimeTo());
+		}
+		//Må ha en for status til møte
+		
+		
+	}
+	
+	
+	//En ikke fungerende funksjon for å sette ikonet i avtalen
+	private ImageIcon getIcon(ArrayList<InvitationModel> invitations){
+		ImageIcon typeIcon;
+		
+		if(invitations.isEmpty()){
+			typeIcon = new ImageIcon("src/resources/avtaleMini.png");
+		}
+		else if(invitations.contains("Venter")){
+			typeIcon = new ImageIcon("src/resources/venterMini.png");
+		}
+		else if(invitations.contains("Avslått")){
+			typeIcon = new ImageIcon("src/resources/avslattMini.png");
+		}
+		else{
+			typeIcon = new ImageIcon("src/resources/godkjentMini.png");
+		}
+		
+		return typeIcon;
+	}
+
+	
+	//lager og returnerer et stringformat av start og slutt av avtalen
 	private String timeToString(Calendar S, Calendar E){
 		String tempString= "";
 		
@@ -93,14 +154,9 @@ public class AppointmentPanel extends JPanel {
 		return tempString;
 	}
 	
-	private void setFont(String font, int size){
-		nameLabel.setFont(new Font(font, 1, size + 3));
-		ownerLabel.setFont(new Font("Dialog", 1, size));
-		timeLabel.setFont(new Font("Dialog", 1, size));
-		iconLabel.setFont(new Font("Dialog", 1, size));
-		locationLabel.setFont(new Font("Dialog", 1, size));
-	}
 	
+	
+	//Setter størrelse på panel + hva som skal vises
 	private void setAppointmentTime(Calendar S, Calendar E){
 		
 		long SMilli = S.getTimeInMillis();
@@ -108,18 +164,14 @@ public class AppointmentPanel extends JPanel {
 		
 		int minutes = (int)((EMilli-SMilli)/60000);
 		
-		System.out.println(minutes);
-		
 		int AppointmentLength = (minutes*WeekView.HOURHEIGHT)/60;
-		
-		System.out.println(AppointmentLength);
-		
+
 		this.setPreferredSize(new Dimension(WeekView.HOURWIDTH - 5, AppointmentLength));
 		this.setMinimumSize(new Dimension(WeekView.HOURWIDTH - 5, AppointmentLength));
 		this.setMaximumSize(new Dimension(WeekView.HOURWIDTH - 5, AppointmentLength));
 
 		
-//Kjapp test av hvordan justering av avtale kan gjÃ¸res LOL!
+		//Kjapp test av hvordan justering av avtale kan gjøres
 		if(minutes == 15){
 			setView(true, false, false,false, false, 10);
 		}
@@ -143,6 +195,7 @@ public class AppointmentPanel extends JPanel {
 
 	}
 	
+	//bestemmer hva som skal vises i panelet + størrelse på skriften
 	private void setView(boolean name, boolean location, boolean time, boolean icon ,boolean owner, int fontSize){
 		nameLabel.setVisible(name);
 		locationLabel.setVisible(location);
@@ -152,13 +205,15 @@ public class AppointmentPanel extends JPanel {
 		setFont("Times New Roman",fontSize);
 	}
 	
-//	public void setModel(Avtale a){
-//		model = a;
-//	}
-//	
-//	public Avtale getModel(){
-//		return model;
-//	}
+	//Setter font og størrelse på alle labelene i panelet
+	private void setFont(String font, int size){
+		//NameLabel vill alltid være 3 hakk større enn de andre labelene (Overskrift)
+		nameLabel.setFont(new Font(font, 1, size + 3));
+		ownerLabel.setFont(new Font("Dialog", 1, size));
+		timeLabel.setFont(new Font("Dialog", 1, size));
+		iconLabel.setFont(new Font("Dialog", 1, size));
+		locationLabel.setFont(new Font("Dialog", 1, size));
+	}
 	
 //	public static void main (String args[]) { 
 //        JFrame frame = new JFrame(""); 
